3895adb8444b6890257564c20f7cdfa618571bf2
Robson <robccorreia@gmail.com>
2022-07-11 20:29:30 -0300

    feat: add theme styled component on project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { ThemeProvider } from "styled-components";
+import { Button } from "./components/Button";
+import { GlobalStyle } from "./components/styles/global";
+import { defaultTheme } from "./components/styles/themes/default";
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
+      <Button />
+      <GlobalStyle />
+    </ThemeProvider>
+  );
+}
